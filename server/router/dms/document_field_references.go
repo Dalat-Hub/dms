effb3a5e7bb49a6a8be77e425293b2dae567e821
Nguyen Trong Hieu <hieuntctk42@gmail.com>
2022-07-20 22:13:07 +0700

    add routers

--- server/router/dms/document_field_references.go
+++ server/router/dms/document_field_references.go
@@ -9,19 +9,19 @@ import (
 type DocumentFieldReferencesRouter struct {
 }
 
-// InitDocumentFieldReferencesRouter 初始化 DocumentFieldReferences 路由信息
+// InitDocumentFieldReferencesRouter Initialize DocumentFieldReferences routing information
 func (s *DocumentFieldReferencesRouter) InitDocumentFieldReferencesRouter(Router *gin.RouterGroup) {
 	documentFieldReferencesRouter := Router.Group("documentFieldReferences").Use(middleware.OperationRecord())
 	documentFieldReferencesRouterWithoutRecord := Router.Group("documentFieldReferences")
 	var documentFieldReferencesApi = v1.ApiGroupApp.DmsApiGroup.DocumentFieldReferencesApi
 	{
-		documentFieldReferencesRouter.POST("createDocumentFieldReferences", documentFieldReferencesApi.CreateDocumentFieldReferences)             // 新建DocumentFieldReferences
-		documentFieldReferencesRouter.DELETE("deleteDocumentFieldReferences", documentFieldReferencesApi.DeleteDocumentFieldReferences)           // 删除DocumentFieldReferences
-		documentFieldReferencesRouter.DELETE("deleteDocumentFieldReferencesByIds", documentFieldReferencesApi.DeleteDocumentFieldReferencesByIds) // 批量删除DocumentFieldReferences
-		documentFieldReferencesRouter.PUT("updateDocumentFieldReferences", documentFieldReferencesApi.UpdateDocumentFieldReferences)              // 更新DocumentFieldReferences
+		documentFieldReferencesRouter.POST("createDocumentFieldReferences", documentFieldReferencesApi.CreateDocumentFieldReferences)
+		documentFieldReferencesRouter.DELETE("deleteDocumentFieldReferences", documentFieldReferencesApi.DeleteDocumentFieldReferences)
+		documentFieldReferencesRouter.DELETE("deleteDocumentFieldReferencesByIds", documentFieldReferencesApi.DeleteDocumentFieldReferencesByIds)
+		documentFieldReferencesRouter.PUT("updateDocumentFieldReferences", documentFieldReferencesApi.UpdateDocumentFieldReferences)
 	}
 	{
-		documentFieldReferencesRouterWithoutRecord.GET("findDocumentFieldReferences", documentFieldReferencesApi.FindDocumentFieldReferences)       // 根据ID获取DocumentFieldReferences
-		documentFieldReferencesRouterWithoutRecord.GET("getDocumentFieldReferencesList", documentFieldReferencesApi.GetDocumentFieldReferencesList) // 获取DocumentFieldReferences列表
+		documentFieldReferencesRouterWithoutRecord.GET("findDocumentFieldReferences", documentFieldReferencesApi.FindDocumentFieldReferences)
+		documentFieldReferencesRouterWithoutRecord.GET("getDocumentFieldReferencesList", documentFieldReferencesApi.GetDocumentFieldReferencesList)
 	}
 }
