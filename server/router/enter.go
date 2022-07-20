0a0cfb98bb3771700247c218786bd3245bf53dae
Nguyen Trong Hieu <hieuntctk42@gmail.com>
2022-07-20 22:50:25 +0700

    add routers

--- server/router/enter.go
+++ server/router/enter.go
@@ -1,13 +1,15 @@
 package router
 
 import (
+	"github.com/flipped-aurora/gin-vue-admin/server/router/dms"
 	"github.com/flipped-aurora/gin-vue-admin/server/router/example"
 	"github.com/flipped-aurora/gin-vue-admin/server/router/system"
 )
 
 type RouterGroup struct {
-	System   system.RouterGroup
-	Example  example.RouterGroup
+	System  system.RouterGroup
+	Example example.RouterGroup
+	Dms     dms.RouterGroup
 }
 
 var RouterGroupApp = new(RouterGroup)
