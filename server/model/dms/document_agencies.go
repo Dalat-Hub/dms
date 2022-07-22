cbcdc744fddf3919307b8ac1e5ab6e7516ad1e9b
DalatCoder <hieuntctk42@gmail.com>
2022-07-22 11:22:32 +0700

    change agency level type to int

--- server/model/dms/document_agencies.go
+++ server/model/dms/document_agencies.go
@@ -9,7 +9,7 @@ type DocumentAgencies struct {
 	Name  string `json:"name" form:"name" gorm:"column:name;"`
 	Code  string `json:"code" form:"code" gorm:"column:code;"`
 	Count int    `json:"count" form:"count" gorm:"column:count;"`
-	Level string `json:"level" form:"level" gorm:"column:level;"`
+	Level int    `json:"level" form:"level" gorm:"column:level;"`
 }
 
 func (DocumentAgencies) TableName() string {
