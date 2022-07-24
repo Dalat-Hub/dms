23b662c7d037df5d6e6e0e53acb6c30c69eb889f
DalatCoder <hieuntctk42@gmail.com>
2022-07-24 10:31:11 +0700

    translate

--- server/model/common/response/response.go
+++ server/model/common/response/response.go
@@ -18,7 +18,6 @@ const (
 )
 
 func Result(code int, data interface{}, msg string, c *gin.Context) {
-	// 开始时间
 	c.JSON(http.StatusOK, Response{
 		code,
 		data,
@@ -27,7 +26,7 @@ func Result(code int, data interface{}, msg string, c *gin.Context) {
 }
 
 func Ok(c *gin.Context) {
-	Result(SUCCESS, map[string]interface{}{}, "操作成功", c)
+	Result(SUCCESS, map[string]interface{}{}, "Thực hiện thao tác thành công", c)
 }
 
 func OkWithMessage(message string, c *gin.Context) {
@@ -35,7 +34,7 @@ func OkWithMessage(message string, c *gin.Context) {
 }
 
 func OkWithData(data interface{}, c *gin.Context) {
-	Result(SUCCESS, data, "查询成功", c)
+	Result(SUCCESS, data, "Thực hiện thao tác thành công", c)
 }
 
 func OkWithDetailed(data interface{}, message string, c *gin.Context) {
@@ -43,7 +42,7 @@ func OkWithDetailed(data interface{}, message string, c *gin.Context) {
 }
 
 func Fail(c *gin.Context) {
-	Result(ERROR, map[string]interface{}{}, "操作失败", c)
+	Result(ERROR, map[string]interface{}{}, "Có lỗi trong quá trình thực hiện thao tác", c)
 }
 
 func FailWithMessage(message string, c *gin.Context) {
