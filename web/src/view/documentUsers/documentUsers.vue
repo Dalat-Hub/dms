d98fc28b28bc96bbbecf0fe3752bad6abbcc6655
DalatCoder <hieuntctk42@gmail.com>
2022-07-29 17:03:09 +0700

    distribute tasks

--- web/src/view/documentUsers/documentUsers.vue
+++ web/src/view/documentUsers/documentUsers.vue
@@ -3,75 +3,87 @@
     <div class="gva-search-box">
       <el-form :inline="true" :model="searchInfo" class="demo-form-inline">
         <el-form-item>
-          <el-button size="small" type="primary" icon="search" @click="onSubmit">查询</el-button>
-          <el-button size="small" icon="refresh" @click="onReset">重置</el-button>
+          <el-button size="small" type="primary" icon="search" @click="onSubmit">Tìm kiếm</el-button>
+          <el-button size="small" icon="refresh" @click="onReset">Đặt lại bộ lọc</el-button>
         </el-form-item>
       </el-form>
     </div>
     <div class="gva-table-box">
-        <div class="gva-btn-list">
-            <el-button size="small" type="primary" icon="plus" @click="openDialog">新增</el-button>
-            <el-popover v-model:visible="deleteVisible" placement="top" width="160">
-            <p>确定要删除吗？</p>
-            <div style="text-align: right; margin-top: 8px;">
-                <el-button size="small" type="primary" link @click="deleteVisible = false">取消</el-button>
-                <el-button size="small" type="primary" @click="onDelete">确定</el-button>
-            </div>
-            <template #reference>
-                <el-button icon="delete" size="small" style="margin-left: 10px;" :disabled="!multipleSelection.length" @click="deleteVisible = true">删除</el-button>
-            </template>
-            </el-popover>
-        </div>
-        <el-table
+      <el-table
         ref="multipleTable"
         style="width: 100%"
         tooltip-effect="dark"
         :data="tableData"
         row-key="ID"
-        @selection-change="handleSelectionChange"
-        >
-        <el-table-column type="selection" width="55" />
-        <el-table-column align="left" label="日期" width="180">
-            <template #default="scope">{{ formatDate(scope.row.CreatedAt) }}</template>
+        :row-class-name="tableRowClassName"
+      >
+        <el-table-column align="left" label="Văn bản" prop="shortTitle" width="300" />
+        <el-table-column align="left" label="Sẽ được hoàn thiện bởi" prop="userId" width="300">
+          <template #default="scope">
+            <span v-if="scope.row.userId"> {{ scope.row?.user.nickName || 'N/A' }}</span>
+            <span v-else style="color: orangered;">Chưa phân bổ</span>
+          </template>
         </el-table-column>
-        <el-table-column align="left" label="documentId字段" prop="documentId" width="120" />
-        <el-table-column align="left" label="userId字段" prop="userId" width="120" />
-        <el-table-column align="left" label="doneAt字段" prop="doneAt" width="120" />
-        <el-table-column align="left" label="按钮组">
-            <template #default="scope">
-            <el-button type="primary" link icon="edit" size="small" class="table-button" @click="updateDocumentUsersFunc(scope.row)">变更</el-button>
-            <el-button type="primary" link icon="delete" size="small" @click="deleteRow(scope.row)">删除</el-button>
-            </template>
+        <el-table-column align="left" label="Xuất bản lúc" width="200">
+          <template #default="scope">
+            <span v-if="scope.row.doneAt"> {{ formatDate(scope.row.doneAt) }}</span>
+            <span v-else style="color: orangered;">N/A</span>
+          </template>
         </el-table-column>
-        </el-table>
-        <div class="gva-pagination">
-            <el-pagination
-            layout="total, sizes, prev, pager, next, jumper"
-            :current-page="page"
-            :page-size="pageSize"
-            :page-sizes="[10, 30, 50, 100]"
-            :total="total"
-            @current-change="handleCurrentChange"
-            @size-change="handleSizeChange"
-            />
-        </div>
+        <el-table-column align="left" label="Tạo lúc" width="200">
+          <template #default="scope">{{ formatDate(scope.row.CreatedAt) }}</template>
+        </el-table-column>
+        <el-table-column align="left" label="Thao tác" width="200" fixed="right">
+          <template #default="scope">
+            <el-button v-if="scope.row.userId != 0" type="primary" link icon="edit" size="small" class="table-button" @click="() => showDocumentUpdate(scope.row)">Cập nhật</el-button>
+            <el-button v-else type="primary" link icon="edit" size="small" @click="() => openDistributionForm(scope.row)">Phân bổ</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="gva-pagination">
+        <el-pagination
+          layout="total, sizes, prev, pager, next, jumper"
+          :current-page="page"
+          :page-size="pageSize"
+          :page-sizes="[10, 30, 50, 100]"
+          :total="total"
+          @current-change="handleCurrentChange"
+          @size-change="handleSizeChange"
+        />
+      </div>
     </div>
-    <el-dialog v-model="dialogFormVisible" :before-close="closeDialog" title="弹窗操作">
-      <el-form :model="formData" label-position="right" ref="elFormRef" :rules="rule" label-width="80px">
-        <el-form-item label="documentId字段:"  prop="documentId" >
-          <el-input v-model.number="formData.documentId" :clearable="true" placeholder="请输入" />
+
+    <el-dialog
+      v-model="distributionFormVisible"
+      :before-close="closeDistributionForm"
+      title="Thêm nhanh văn bản"
+    >
+      <el-form :model="distributionFormData" label-position="right" label-width="150px">
+        <el-form-item label="Tiêu đề">
+          <el-input v-model="distributionFormData.title" readonly clearable />
         </el-form-item>
-        <el-form-item label="userId字段:"  prop="userId" >
-          <el-input v-model.number="formData.userId" :clearable="true" placeholder="请输入" />
-        </el-form-item>
-        <el-form-item label="doneAt字段:"  prop="doneAt" >
-          <el-date-picker v-model="formData.doneAt" type="date" style="width:100%" placeholder="选择日期" :clearable="true"  />
+        <el-form-item label="Gắn thẻ người dùng">
+          <el-select
+            v-model="distributionFormData.relatedUsers"
+            :style="{ width: '100%' }"
+            clearable
+            filterable
+            multiple
+            placeholder="Chọn người dùng liên quan"
+          >
+            <el-option
+              v-for="item in usersOptions"
+              :key="item.ID"
+              :label="item.nickName"
+              :value="item.ID"
+            />
+          </el-select>
         </el-form-item>
       </el-form>
       <template #footer>
         <div class="dialog-footer">
-          <el-button size="small" @click="closeDialog">取 消</el-button>
-          <el-button size="small" type="primary" @click="enterDialog">确 定</el-button>
+          <el-button size="small" @click="closeDistributionForm">Đóng</el-button>
+          <el-button size="small" type="primary" @click="enterDistributionForm">Phân bổ</el-button>
         </div>
       </template>
     </el-dialog>
@@ -86,65 +98,61 @@ export default {
 
 <script setup>
 import {
-  createDocumentUsers,
-  deleteDocumentUsers,
-  deleteDocumentUsersByIds,
-  updateDocumentUsers,
-  findDocumentUsers,
-  getDocumentUsersList
+  getDocumentUsersList,
+  distributeTasks
 } from '@/api/documentUsers'
+import { getUserList } from '../../api/user'
 
-// 全量引入格式化工具 请按需保留
-import { getDictFunc, formatDate, formatBoolean, filterDict } from '@/utils/format'
-import { ElMessage, ElMessageBox } from 'element-plus'
-import { ref, reactive } from 'vue'
+import { formatDate } from '@/utils/format'
+import { ref } from 'vue'
 
-// 自动化生成的字典（可能为空）以及字段
-const formData = ref({
-        documentId: 0,
-        userId: 0,
-        doneAt: new Date(),
-        })
+import { ElMessage } from 'element-plus'
+import { useRouter } from 'vue-router'
 
-// 验证规则
-const rule = reactive({
-})
+const router = useRouter()
 
-const elFormRef = ref()
-
-
-// =========== 表格控制部分 ===========
 const page = ref(1)
 const total = ref(0)
 const pageSize = ref(10)
 const tableData = ref([])
 const searchInfo = ref({})
+const usersOptions = ref([])
 
-// 重置
+const distributionFormData = ref({
+  id: '',
+  title: '',
+  relatedUsers: []
+})
+const distributionFormVisible = ref(false)
+const currentDocumentTask = ref(null)
+
 const onReset = () => {
   searchInfo.value = {}
 }
 
-// 搜索
 const onSubmit = () => {
   page.value = 1
   pageSize.value = 10
   getTableData()
 }
 
-// 分页
 const handleSizeChange = (val) => {
   pageSize.value = val
   getTableData()
 }
 
-// 修改页面容量
 const handleCurrentChange = (val) => {
   page.value = val
   getTableData()
 }
 
-// 查询
+const loadUserOptions = async() => {
+  const table = await getUserList({ page: 1, pageSize: 1000 })
+  if (table.code === 0) {
+    usersOptions.value = table.data.list
+  }
+}
+
 const getTableData = async() => {
   const table = await getDocumentUsersList({ page: page.value, pageSize: pageSize.value, ...searchInfo.value })
   if (table.code === 0) {
@@ -156,141 +164,69 @@ const getTableData = async() => {
 }
 
 getTableData()
+loadUserOptions()
 
-// ============== 表格控制部分结束 ===============
+const tableRowClassName = (data, rowIndex) => {
+  if (data.row.userId === 0) { return 'danger-row' }
 
-// 获取需要的字典 可能为空 按需保留
-const setOptions = async () =>{
+  if (data.row.doneAt) { return 'success-row' }
+
+  return 'warning-row'
 }
 
-// 获取需要的字典 可能为空 按需保留
-setOptions()
-
-
-// 多选数据
-const multipleSelection = ref([])
-// 多选
-const handleSelectionChange = (val) => {
-    multipleSelection.value = val
+const closeDistributionForm = () => {
+  distributionFormVisible.value = false
 }
 
-// 删除行
-const deleteRow = (row) => {
-    ElMessageBox.confirm('确定要删除吗?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-    }).then(() => {
-            deleteDocumentUsersFunc(row)
-        })
+const openDistributionForm = (documentTask) => {
+  distributionFormVisible.value = true
+  currentDocumentTask.value = documentTask
+  distributionFormData.value.title = documentTask.shortTitle
+  distributionFormData.value.id = documentTask.ID
+}
+
+const enterDistributionForm = async() => {
+  const distribution = {
+    id: distributionFormData.value.id,
+    documentId: 0,
+    userIds: distributionFormData.value.relatedUsers
+  }
+
+  const response = await distributeTasks(distribution)
+
+  if (response.code === 0) {
+    ElMessage({
+      type: 'success',
+      message: 'Phân bổ người dùng thành công'
+    })
+
+    distributionFormData.value.id = null
+    distributionFormData.value.relatedUsers = []
+    distributionFormData.value.title = ''
+
+    getTableData()
+  }
+}
+
+const showDocumentUpdate = (task) => {
+  router.push({
+    name: 'documents-update',
+    params: {
+      id: task.documentId
     }
-
-
-// 批量删除控制标记
-const deleteVisible = ref(false)
-
-// 多选删除
-const onDelete = async() => {
-      const ids = []
-      if (multipleSelection.value.length === 0) {
-        ElMessage({
-          type: 'warning',
-          message: '请选择要删除的数据'
-        })
-        return
-      }
-      multipleSelection.value &&
-        multipleSelection.value.map(item => {
-          ids.push(item.ID)
-        })
-      const res = await deleteDocumentUsersByIds({ ids })
-      if (res.code === 0) {
-        ElMessage({
-          type: 'success',
-          message: '删除成功'
-        })
-        if (tableData.value.length === ids.length && page.value > 1) {
-          page.value--
-        }
-        deleteVisible.value = false
-        getTableData()
-      }
-    }
-
-// 行为控制标记（弹窗内部需要增还是改）
-const type = ref('')
-
-// 更新行
-const updateDocumentUsersFunc = async(row) => {
-    const res = await findDocumentUsers({ ID: row.ID })
-    type.value = 'update'
-    if (res.code === 0) {
-        formData.value = res.data.redocumentUsers
-        dialogFormVisible.value = true
-    }
+  })
 }
 
-
-// 删除行
-const deleteDocumentUsersFunc = async (row) => {
-    const res = await deleteDocumentUsers({ ID: row.ID })
-    if (res.code === 0) {
-        ElMessage({
-                type: 'success',
-                message: '删除成功'
-            })
-            if (tableData.value.length === 1 && page.value > 1) {
-            page.value--
-        }
-        getTableData()
-    }
-}
-
-// 弹窗控制标记
-const dialogFormVisible = ref(false)
-
-// 打开弹窗
-const openDialog = () => {
-    type.value = 'create'
-    dialogFormVisible.value = true
-}
-
-// 关闭弹窗
-const closeDialog = () => {
-    dialogFormVisible.value = false
-    formData.value = {
-        documentId: 0,
-        userId: 0,
-        doneAt: new Date(),
-        }
-}
-// 弹窗确定
-const enterDialog = async () => {
-     elFormRef.value?.validate( async (valid) => {
-             if (!valid) return
-              let res
-              switch (type.value) {
-                case 'create':
-                  res = await createDocumentUsers(formData.value)
-                  break
-                case 'update':
-                  res = await updateDocumentUsers(formData.value)
-                  break
-                default:
-                  res = await createDocumentUsers(formData.value)
-                  break
-              }
-              if (res.code === 0) {
-                ElMessage({
-                  type: 'success',
-                  message: '创建/更改成功'
-                })
-                closeDialog()
-                getTableData()
-              }
-      })
-}
 </script>
 
 <style>
+.el-table .warning-row {
+  background-color: #fdf6ec;
+}
+.el-table .danger-row {
+  background-color: #ffeff1;
+}
+.el-table .success-row {
+  background-color: #f0f9eb;
+}
 </style>
