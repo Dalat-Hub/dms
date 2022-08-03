7bd66da3c20a9a5a40588afe5af1305fcb1379c2
DalatCoder <hieuntctk42@gmail.com>
2022-08-03 16:32:53 +0700

    check if client receive new jwt token

--- web/src/view/login/index.vue
+++ web/src/view/login/index.vue
@@ -143,27 +143,38 @@ const notYetInit = ref(true)
 
 const checkLoginToken = async() => {
   const query = route.query
+  let hasValue = false
 
-  if (!query.redirect) return
-  if (!query.redirect.includes('token=')) return
+  if (query.redirect) {
+    if (!query.redirect.includes('token=')) return
 
-  const parts = query.redirect.split('token=')
-  const token = parts[1]
+    const parts = query.redirect.split('token=')
+    const token = parts[1]
 
-  if (!token) return
+    if (!token) return
 
-  userStore.setToken(token)
-  const res = await userStore.GetUserInfo()
+    userStore.setToken(token)
+    hasValue = true
+  }
 
-  if (res.code === 0) {
-    window.location.reload()
-  } else {
-    ElMessage({
-      type: 'error',
-      message: 'Thông tin đăng nhập không hợp lệ',
-      showClose: true,
-      duration: 5000
-    })
+  if (query.token) {
+    userStore.setToken(query.token)
+    hasValue = true
+  }
+
+  if (hasValue) {
+    const res = await userStore.GetUserInfo()
+
+    if (res.code === 0) {
+      window.location.reload()
+    } else {
+      ElMessage({
+        type: 'error',
+        message: 'Thông tin đăng nhập không hợp lệ',
+        showClose: true,
+        duration: 5000
+      })
+    }
   }
 }
 checkLoginToken()
