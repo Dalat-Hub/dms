58873c5f8914c7f998ba662514756d89b8c9cb9c
DalatCoder <hieuntctk42@gmail.com>
2022-08-11 16:15:45 +0700

    add document count for field

--- server/service/dms/document_field_references.go
+++ server/service/dms/document_field_references.go
@@ -57,3 +57,50 @@ func (documentFieldReferencesService *DocumentFieldReferencesService) GetDocumen
 	err = db.Limit(limit).Offset(offset).Find(&documentFieldReferencess).Error
 	return documentFieldReferencess, total, err
 }
+
+type fieldStats struct {
+	FieldId uint `json:"field_id"`
+	Count   uint `json:"count"`
+}
+
+func (documentFieldReferencesService *DocumentFieldReferencesService) AttachDocumentCount(fields []*dms.DocumentFields) (err error) {
+	documents := make([]dms.Documents, 0)
+
+	err = global.GVA_DB.Model(&dms.Documents{}).
+		Where("type = ? AND status = ?", dms.TYPE_DOCUMENT, dms.STATUS_PUBLISHED).
+		Find(&documents).Error
+
+	if err != nil {
+		return err
+	}
+
+	documentIds := make([]uint, 0)
+	for _, v := range documents {
+		documentIds = append(documentIds, v.ID)
+	}
+
+	results := make([]fieldStats, 0)
+
+	err = global.GVA_DB.Model(&dms.DocumentFieldReferences{}).
+		Select("field_id, count(id) as count").
+		Where("document_id IN ?", documentIds).
+		Group("field_id").
+		Find(&results).Error
+
+	if err != nil {
+		return err
+	}
+
+	resultsAsMap := map[uint]uint{}
+	for _, stat := range results {
+		resultsAsMap[stat.FieldId] = stat.Count
+	}
+
+	for _, field := range fields {
+		if val, ok := resultsAsMap[field.ID]; ok {
+			field.Count = int(val)
+		}
+	}
+
+	return nil
+}
