175dd487ed08e9c64da3b4e0e390fd210994a2b6
DalatCoder <hieuntctk42@gmail.com>
2022-08-16 10:01:57 +0700

    change search stats content

--- web-customer/src/components/SearchStat.vue
+++ web-customer/src/components/SearchStat.vue
@@ -1,28 +1,12 @@
 <template>
   <div>
     <p>
-      <span v-if="count > 0">Tìm thấy {{ count }} văn bản </span>
-      <span v-else>Không tìm thấy văn bản </span>
-
-      <span v-if="Object.keys(searchBy).length === 0">trên hệ thống</span
-      ><span v-else>thuộc:</span>
-    </p>
-
-    <p v-if="searchBy.signText">
-      Từ khóa:
-      <el-tag class="ml-2" type="danger">{{ this.searchBy.signText }}</el-tag>
-    </p>
-    <p v-if="searchBy.agencyId">
-      Cơ quan ban hành:
-      <el-tag class="ml-2" type="success">{{ this.getAgencyTitle() }}</el-tag>
-    </p>
-    <p v-if="searchBy.categoryId">
-      Thể loại:
-      <el-tag class="ml-2" type="info">{{ this.getCategoryTitle() }}</el-tag>
-    </p>
-    <p v-if="searchBy.fieldId">
-      Lĩnh vực:
-      <el-tag class="ml-2" type="warning">{{ this.getFieldTitle() }}</el-tag>
+      <span v-if="count > 0"
+        >Tìm thấy
+        <el-tag class="ml-2" type="danger">{{ count }}</el-tag>
+        văn bản trên hệ thống
+      </span>
+      <span v-else>Không tìm thấy văn bản trên hệ thống</span>
     </p>
   </div>
 </template>
