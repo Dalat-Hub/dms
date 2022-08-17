fb2b8561598f192929a0155f992e0550b9648bfc
DalatCoder <hieuntctk42@gmail.com>
2022-08-17 09:04:40 +0700

    add login page

---
+++ web-customer/src/views/LoginView.vue
@@ -0,0 +1,95 @@
+<template>
+  <el-row justify="center" class="login-form">
+    <el-col :sm="12" :lg="10">
+      <el-form :model="form" label-width="120px" @keyup.enter="onSubmit">
+        <el-form-item label="Tên đăng nhập">
+          <el-input
+            v-model="form.username"
+            placeholder="Vui lòng điền tên đăng nhập"
+            autocomplete="off"
+          />
+        </el-form-item>
+        <el-form-item label="Mật khẩu">
+          <el-input
+            v-model="form.password"
+            placeholder="Vui lòng điền mật khẩu"
+            autocomplete="off"
+            type="password"
+          />
+        </el-form-item>
+        <el-form-item label="Mã xác thực">
+          <el-input
+            v-model="form.captcha"
+            placeholder="Vui lòng điền mã xác thực"
+            style="width: 60%"
+          />
+          <img
+            v-if="picPath"
+            :src="picPath"
+            alt="Vui lòng nhập mã bảo mật"
+            @click="loginVerify()"
+            style="width: 40%"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">Đăng nhập</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
+
+<script setup>
+import { reactive, ref } from "vue";
+import { captcha } from "@/api/user";
+import { useUserStore } from "@/pinia/modules/user";
+import router from "@/router/index";
+
+const userStore = useUserStore();
+
+const picPath = ref("");
+const form = reactive({
+  username: "",
+  password: "",
+  captcha: "",
+  captchaId: "",
+});
+
+const login = async () => {
+  return await userStore.LoginIn(form);
+};
+
+const loginVerify = () => {
+  captcha({}).then((ele) => {
+    picPath.value = ele.data.picPath;
+    form.captchaId = ele.data.captchaId;
+  });
+};
+loginVerify();
+
+const onSubmit = async () => {
+  const flag = await login();
+  if (!flag) {
+    loginVerify();
+  } else {
+    userStore.GetUserInfo().then(() => {
+      router.push({
+        path: "/",
+      });
+    });
+  }
+};
+</script>
+
+<style scoped>
+.login-form {
+  padding-top: 3rem;
+  padding-bottom: 20rem;
+}
+</style>
