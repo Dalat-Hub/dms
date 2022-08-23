a2b7225a3ab437eb5062530b0e4621c9571069d8
DalatCoder <hieuntctk42@gmail.com>
2022-08-23 17:13:37 +0700

    set login token

--- web-customer/src/views/SetToken.vue
+++ web-customer/src/views/SetToken.vue
@@ -1,26 +1,6 @@
 <template>
   <div style="margin: 15rem 0">
-    <h3 v-if="loginSuccess === null">Tiến hành đăng nhập...</h3>
-
-    <div v-if="loginSuccess === false">
-      <h1>Có lỗi trong quá trình đăng nhập</h1>
-    </div>
-
-    <div v-if="loginSuccess === true">
-      <h1>Đăng nhập thành công</h1>
-
-      <div style="margin-top: 1rem; margin-bottom: 1rem">
-        <el-button type="primary" plain @click="handleOnHomePageClick"
-          >Trang chủ</el-button
-        >
-      </div>
-
-      <div>
-        <el-button type="success" plain @click="handleOnAdminPageClick"
-          >Trang quản trị</el-button
-        >
-      </div>
-    </div>
+    <h3>Tiến hành đăng nhập...</h3>
   </div>
 </template>
 
@@ -34,42 +14,27 @@ export default {
 import { useRoute } from "vue-router";
 import { useUserStore } from "@/pinia/modules/user";
 import { useRouter } from "vue-router";
-import { onMounted, ref } from "vue";
+import { onMounted } from "vue";
 
 const route = useRoute();
 const userStore = useUserStore();
 const router = useRouter();
 
-const loginSuccess = ref(null);
-
 const checkLoginToken = async () => {
   const existingToken = userStore.token || null;
   if (existingToken) {
-    loginSuccess.value = true;
+    router.push({ name: "home" });
     return;
   }
 
   const token = route.query.token || null;
   if (!token) {
-    loginSuccess.value = false;
+    router.push({ name: "home" });
     return;
   }
 
   userStore.setToken(route.query.token);
   await userStore.GetUserInfo();
-
-  loginSuccess.value = true;
-};
-
-const handleOnHomePageClick = () => {
-  router.push({ name: "home" });
-};
-
-const handleOnAdminPageClick = () => {
-  window.open(
-    `${process.env.VUE_APP_ADMIN_URL}/#/login?token=${userStore.token}`,
-    "_blank"
-  );
 };
 
 onMounted(() => {
