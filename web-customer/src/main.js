114f33cae67720d2cf7b1c886e91923ea220f79a
DalatCoder <hieuntctk42@gmail.com>
2022-08-30 14:46:40 +0700

    add vn locale

--- web-customer/src/main.js
+++ web-customer/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import viVN from "element-plus/es/locale/lang/vi";
 import App from "./App.vue";
 import { store } from "@/pinia";
 import router from "./router";
@@ -8,7 +9,7 @@ import router from "./router";
 const app = createApp(App);
 
 app.use(store);
-app.use(ElementPlus);
+app.use(ElementPlus, { locale: viVN });
 app.use(router);
 
 app.mount("#app");
