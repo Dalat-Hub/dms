56e6b29155b5332968811dd38d80e6213c459006
DalatCoder <hieuntctk42@gmail.com>
2022-09-06 14:38:08 +0700

    change type

--- server/model/dms/document_users.go
+++ server/model/dms/document_users.go
@@ -9,8 +9,8 @@ import (
 type DocumentUsers struct {
 	global.GVA_MODEL
 	DocumentId uint      `json:"documentId" form:"documentId" gorm:"column:document_id;size:19;"`
-	Title      string    `json:"title" form:"title" gorm:"column:title;"`
-	ShortTitle string    `json:"shortTitle" form:"shortTitle" gorm:"column:short_title;"`
+	Title      string    `json:"title" form:"title" gorm:"column:title;type:text"`
+	ShortTitle string    `json:"shortTitle" form:"shortTitle" gorm:"column:short_title;type:text"`
 	SignText   string    `json:"signText" form:"signText" gorm:"column:sign_text;"`
 	UserId     uint      `json:"userId" form:"userId" gorm:"column:user_id;size:19;"`
 	DoneAt     null.Time `json:"doneAt" form:"doneAt" gorm:"column:done_at;"`
